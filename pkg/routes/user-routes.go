e1b087d8e233e7c02fa595a63935a8429ffe6fa2
Anwa Pohtae <anwa.p2544@gmail.com>
2024-02-20 17:48:55 +0700

    20022024

--- pkg/routes/user-routes.go
+++ pkg/routes/user-routes.go
@@ -2,20 +2,20 @@ package routes
 
 import (
 	"api/pkg/controllers"
+
 	"github.com/gin-gonic/gin"
 )
 
 var RegisterUserRoutes = func(router *gin.Engine) {
 	api := router.Group("/api/user")
 	{
-		// api.Use(verifyToken)
 		api.POST("/register", controllers.CreateUser)
 		api.POST("/login", controllers.Login)
-		api.GET("/", controllers.GetUser)
+		api.GET("/", controllers.GetUser, verifyToken)
 		api.GET("/:userId", controllers.GetUserById)
 		// api.GET("/:email", controllers.GetUserByEmail)
-		api.PUT("/:userId", controllers.UpdateUser)
-		api.DELETE("/:userId", controllers.DeleteUser)
+		api.PUT("/:userId", controllers.UpdateUser, verifyToken)
+		api.DELETE("/:userId", controllers.DeleteUser, verifyToken)
 	}
 
 }
